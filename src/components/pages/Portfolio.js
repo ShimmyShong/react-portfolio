394114fc09f52be3fa3b9023cfcda2b0dacf1e1a
ShimmyShong <lattabun@gmail.com>
2023-08-07 12:27:06 -0400

    readme updated

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import '../../styles/Body.css'
+import Coolectibles from '../../images/Project22.png'
+import outNAbout from '../../images/outnabout.png'
+import weatherApp from '../../images/chrome_fzmKja5Eqe.png'
+import quizSite from '../../images/chrome_2Aln1M73pI.png'
+import gitHub from '../../images/github-mark.png'
 
 export default function Portfolio() {
     return (
@@ -7,12 +12,12 @@ export default function Portfolio() {
             <h1 className='title'>Portfolio</h1>
             <div className='grid'>
                 <div className='project'>
-                    <img className='project-img' src='https://via.placeholder.com/300' alt='Project 1' />
+                    <img className='project-img' src={Coolectibles} alt='Coolectibles' />
                     <div className='project-info'>
-                        <a href='#' alt='deployed project'>Project Name</a>
-                        <a href='#' target='_blank' rel='noopener noreferrer'>
+                        <a href='https://blooming-retreat-76948.herokuapp.com/' alt='deployed project'>Coolectibles</a>
+                        <a href='https://github.com/ds055/collectible-database' target='_blank' rel='noopener noreferrer'>
                             <img
-                                src='https://via.placeholder.com/30'
+                                src={gitHub}
                                 alt='GitHub Repository'
                                 className='github-icon'
                             />
@@ -21,12 +26,12 @@ export default function Portfolio() {
                 </div>
 
                 <div className='project'>
-                    <img className='project-img' src='https://via.placeholder.com/300' alt='Project 2' />
+                    <img className='project-img' src={outNAbout} alt="Out 'n' About" />
                     <div className='project-info'>
-                        <a href='#' alt='deployed project'>Project Name</a>
-                        <a href='#' target='_blank' rel='noopener noreferrer'>
+                        <a href='https://jtpheonix13.github.io/stunning-guacamole/' alt='deployed project'>Out 'n' About</a>
+                        <a href='https://github.com/jtpheonix13/stunning-guacamole' target='_blank' rel='noopener noreferrer'>
                             <img
-                                src='https://via.placeholder.com/30'
+                                src={gitHub}
                                 alt='GitHub Repository'
                                 className='github-icon'
                             />
@@ -35,12 +40,12 @@ export default function Portfolio() {
                 </div>
 
                 <div className='project'>
-                    <img className='project-img' src='https://via.placeholder.com/300' alt='Project 3' />
+                    <img className='project-img' src={weatherApp} alt='Weather App' />
                     <div className='project-info'>
-                        <a href='#' alt='deployed project'>Project Name</a>
-                        <a href='#' target='_blank' rel='noopener noreferrer'>
+                        <a href='https://shimmyshong.github.io/weather-dashboard/' alt='deployed project'>Weather App</a>
+                        <a href='https://github.com/ShimmyShong/weather-dashboard' target='_blank' rel='noopener noreferrer'>
                             <img
-                                src='https://via.placeholder.com/30'
+                                src={gitHub}
                                 alt='GitHub Repository'
                                 className='github-icon'
                             />
@@ -49,12 +54,12 @@ export default function Portfolio() {
                 </div>
 
                 <div className='project'>
-                    <img className='project-img' src='https://via.placeholder.com/300' alt='Project 4' />
+                    <img className='project-img' src={quizSite} alt='Quiz Site' />
                     <div className='project-info'>
-                        <a href='#' alt='deployed project'>Project Name</a>
-                        <a href='#' target='_blank' rel='noopener noreferrer'>
+                        <a href='https://shimmyshong.github.io/basic-quiz-website/' alt='deployed project'>Quiz Site</a>
+                        <a href='https://github.com/ShimmyShong/basic-quiz-website' target='_blank' rel='noopener noreferrer'>
                             <img
-                                src='https://via.placeholder.com/30'
+                                src={gitHub}
                                 alt='GitHub Repository'
                                 className='github-icon'
                             />
